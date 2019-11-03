c2906e1fb53386575e3c77ffbd33c85b9c4fa192
pureguin <pure.bluelove@hotmail.co.th>
2019-11-03 16:02:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/appbaby/AlphabetEng.java
@@ -0,0 +1,14 @@
+package com.example.appbaby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class alphabet_eng extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabet_eng);
+    }
+}
---
+++ app/src/main/java/com/example/appbaby/AppDetailsActivity.java
@@ -0,0 +1,4 @@
+package com.example.appbaby;
+
+public class AppDetailsActivity {
+}
