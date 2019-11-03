542e32573ac1f5ef3b2ee9275763ec35a7ebc112
pureguin <pure.bluelove@hotmail.co.th>
2019-11-03 21:49:43 +0700

    add alphabet thai

---
+++ app/src/main/java/com/example/appbaby/AlphabetThai.java
@@ -0,0 +1,14 @@
+package com.example.appbaby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlphabetThai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabet_thai);
+    }
+}
